15ffb22bb380fe8def11a0403c1e5c0e8799a7c7
KxngBari <49103177+KxngBari@users.noreply.github.com>
2022-04-26 10:44:19 +0100

    Added colour and movement

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -6,11 +6,28 @@ public class Nematode : MonoBehaviour
 {
     public int length = 5;
 
+    [SerializeField]
+    Gradient gradient;
+
     public Material material;
 
     void Awake()
     {
-        // Put your code here!
+        for (int i = 0; i < length; i++)
+        {
+            float value = Mathf.Lerp(0f, 1f, (i/(float)length));
+            Color color = gradient.Evaluate(value);
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = transform.position - transform.forward *i;
+            sphere.transform.parent = transform;
+            var sphereRenderer = sphere.GetComponent<Renderer>();
+            sphereRenderer.material.SetColor("_Color", color);
+
+            if (i == 0)
+            {
+                sphere.AddComponent(typeof(NoiseWander));
+            }
+        }
     }
 
 
